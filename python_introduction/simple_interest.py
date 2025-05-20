286d49a9be2f4e800f08ee7b507f7d9993cb38c9
itsaiight <mahlaregosiame@gmail.com>
2025-05-20 14:15:20 +0200

    Made changes to formula in simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,8 +3,8 @@ rate = 0.05  # Rate of interest 5%
 time = 3  # Time in years
 
 # Simple Interest formula: I = P * R * T
-I = principal * rate * time
-interest = I
+interest = principal * rate * time
+
 
 # Print the result  
 print("The simple interest is:", interest)
