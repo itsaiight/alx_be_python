a2c9bf9e831ad39c8b1ead17d78f92bfc73e83a1
itsaiight <mahlaregosiame@gmail.com>
2025-06-10 21:04:21 +0200

    Updated bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,9 +15,10 @@ class BankAccount:
             return True
         return False
 
-    def display_balance(self):
-        print(f"Current balance: ${self.balance}")
-        return self.balance        
+    def display_balance(self, current_balance=0):
+        current_balance = self.balance if current_balance == 0 else current_balance
+        print(f"Current Balance: ${current_balance:.2f}")
+        return self.balance
         
 
     def __str__(self):
